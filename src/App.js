0c6e5bd939cf15d38aa19f0041f472d30704e8b1
Megan Penn <penn.meg@gmail.com>
2021-10-07 20:30:49 -0400

    Remove unused styles and code and set up sections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button } from "./components/Button.tsx";
+import { Sidebar } from "./sections/Sidebar.tsx";
+import "./App.css";
 
 function App() {
+  const [isSelected, setIsSelected] = useState("sidebar--typography");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Sidebar selected={isSelected} callback={(id) => setIsSelected(id)} />
+      <div className="section">
+        {isSelected === "sidebar--button" && (
+          <Button theme="Primary" text="Hello World" />
+        )}
+      </div>
     </div>
   );
 }
